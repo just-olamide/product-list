8d50fec2b29157c4e6b5e77d92b0d1029f31da7f
zaynab <holamideh8@gmail.com>
2025-04-16 10:11:37 +0100

    changes in the body

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ async function fetchProduct() {
     const response = await fetch("data.json");
     data = await response.json(); // Store fetched data in the 'data' variable
     console.log(data);
-    let counter = 0;
+    let counter = 1;
 
     data.forEach((element) => {
-   
-
       console.log(counter);
 
       let cart = document.createElement("div");
       cart.classList.add("cart");
       let cartProduct = document.createElement("div");
+      let cartTwo = document.createElement("div");
       cartProduct.innerHTML = `<div id="images">
                 <img id="img" src="${element.image.desktop}" >
       <button id="btncart"><img id="decrease" src="./assets/images/icon-decrement-quantity.svg"><span id="counter">${counter}</span><img id="increase" src="./assets/images/icon-increment-quantity.svg"></button>
@@ -32,18 +31,33 @@ async function fetchProduct() {
 
         cartProduct.querySelector("#btncart").style.display = "block";
         cartProduct.querySelector("#btncart").style.display = "flex";
+        cartProduct.querySelector("#counter").innerHTML = counter;
+        console.log(counter);
+        document.getElementById('cake').style.display = 'none'
+        cartTwo.innerHTML = ` <h5> ${element.name}</h5>
+
+    <p>${(counter = 1)}x ${element.price.toFixed(
+          2
+        )}<span><img src="./assets/images/icon-remove-item.svg" alt=""></span></p>`;
       });
 
       cartProduct.querySelector("#decrease").addEventListener("click", () => {
-        if(counter === 1){
-cartProduct.querySelector("#btn").style.display = "block";
-cartProduct.querySelector("#btncart").style.display = "none";
-return
-        } 
-        
+        if (counter === 1) {
+          cartProduct.querySelector("#btn").style.display = "block";
+          cartProduct.querySelector("#btncart").style.display = "none";
+          cartTwo.style.display = "none";
+        document.getElementById('cake').style.display = 'block'
+
+          return;
+        }
+
         counter--;
         cartProduct.querySelector("#counter").innerHTML = counter;
+        cartTwo.innerHTML = ` <h3> ${element.name}</h3>
 
+        <p>${counter}x ${element.price.toFixed(
+          2
+        )}<span><img src="./assets/images/icon-remove-item.svg" alt=""></span></p>`;
         console.log(counter);
       });
 
@@ -51,9 +65,15 @@ return
         counter++;
         cartProduct.querySelector("#counter").innerHTML = counter;
         console.log(counter);
+        cartTwo.innerHTML = ` <h3> ${element.name}</h3>
+
+    <p>${counter}x ${element.price.toFixed(
+          2
+        )}<span><img src="./assets/images/icon-remove-item.svg" alt=""></span></p>`;
       });
       console.log(counter);
-      
+
+      document.getElementById("cart3").appendChild(cartTwo);
       cart.appendChild(cartProduct);
       product.appendChild(cart);
       // const
